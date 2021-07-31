83ffc9c8788f48ebd8857b525f79d25af5dc254d
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2021-07-31 20:31:56 -0300

    Service discovery

---
+++ serivce-discovery/src/main/java/com/matheuscordeiro/serivcediscovery/SerivceDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.matheuscordeiro.serivcediscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerivceDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SerivceDiscoveryApplication.class, args);
+	}
+
+}
---
+++ serivce-discovery/src/test/java/com/matheuscordeiro/serivcediscovery/SerivceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheuscordeiro.serivcediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerivceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
